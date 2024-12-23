d3bf4713c2d74a80f570d13183580da738c54f73
rannyzita <fs.vitoria.soares@gmail.com>
2024-12-23 00:02:04 -0300

    criando o controller de usuario, para depois ver se ta funcionando a entrada de dados, pela route

---
+++ src/controllers /UsuarioController.js
@@ -0,0 +1,70 @@
+const UsuarioRepository = require('../models/UsuarioRepository');
+
+class UsuarioController {
+  // Método para listar todos os usuários
+static async getAllUsuarios(req, res) {
+    try {
+        const usuarios = await UsuarioRepository.findAll();
+        res.json(usuarios);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+}
+
+  // Método para buscar um usuário por ID
+static async getUsuarioById(req, res) {
+    try {
+        const usuario = await UsuarioRepository.findById(req.params.id);
+        if (usuario) {
+            res.json(usuario);
+        } else {
+            res.status(404).json({ message: 'Usuário não encontrado' });
+        }
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+}
+
+  // Método para criar um novo usuário
+static async createUsuario(req, res) {
+    try {
+        const newUsuario = await UsuarioRepository.create(req.body);
+        // status usado para indicar que algum recurso foi criado com sucesso
+        res.status(201).json(newUsuario);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+}
+
+  // Método para atualizar um usuário
+static async updateUsuario(req, res) {
+    try {
+        const updatedUsuario = await UsuarioRepository.update(req.params.id, req.body);
+        if (updatedUsuario) {
+            res.json(updatedUsuario);
+        } else {
+            res.status(404).json({ message: 'Usuário não encontrado' });
+        }
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+}
+
+  // Método para deletar um usuário
+static async deleteUsuario(req, res) {
+    try {
+        const deletedUsuario = await UsuarioRepository.delete(req.params.id);
+        if (deletedUsuario) {
+            // status 204 indica que algo foi realizada
+            // mas sem retorno de algo
+            res.status(204).end();
+        } else {
+            res.status(404).json({ message: 'Usuário não encontrado' });
+        }
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+}
+}
+
+module.exports = UsuarioController;
