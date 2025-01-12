f480385d7782352b897071a72dafc849df361a16
rannyzita <fs.vitoria.soares@gmail.com>
2025-01-12 00:49:43 -0300

    finalizando a rota de favoritos

--- src/routes/PostFavoritoRoute.js
+++ src/routes/PostFavoritoRoute.js
@@ -1,3 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const PostFavoritoController = require('../controllers/PostFavoritoController.js')
+
+// Usando os métodos do controller nas rotas
+router.post('/', PostFavoritoController.createPostFavorito);
+router.get('/:id', PostFavoritoController.getAllFavoritosByUserId);
+router.delete('/:id', PostFavoritoController.deletePostFavorito);
+
+module.exports = router;
