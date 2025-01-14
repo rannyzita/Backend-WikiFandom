e99f376784551e5b947143f940830a3a9a06dbb7
Kariny <karinysilva.estudo@gmail.com>
2025-01-14 16:35:59 -0300

    Implementando ImagemController com métodos para criar e deletar imagem

---
+++ src/controllers/ImagemController.js
@@ -0,0 +1,27 @@
+const ImagemRepository = require('../models/ImagemRepository.js');
+
+class ImagemRepository{
+    static async getAllImage(req, res){
+        try{
+
+        }catch(erro){
+
+        }
+    }
+
+    static async createImagem(req, res){
+        try{
+
+        } catch(erro){
+
+        }
+    }
+
+    static async deleteImagem(req, res){
+        try{
+
+        } catch(erro){
+            
+        }
+    }
+}
