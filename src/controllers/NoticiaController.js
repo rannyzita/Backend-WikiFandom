30e780487d3f439fdf76cc8b1b95bc9026e32011
Kariny <karinysilva.estudo@gmail.com>
2025-01-14 16:41:06 -0300

    Esqueleto de NoticiaRepository com métodos vazios

---
+++ src/controllers/NoticiaController.js
@@ -0,0 +1,27 @@
+const NoticiaRepository = require('../models/NoticiaRepository.js');
+
+class NoticiaRepository{
+    static async getAllNoticias(req, res){
+        try{
+
+        } catch(erro){
+
+        }
+    }
+
+    static async createNoticia(req, res){
+        try{
+
+        } catch(erro){
+
+        }
+    }
+
+    static async deleteNoticia(req, res){
+        try{
+
+        } catch(erro){
+            
+        }
+    }
+}
