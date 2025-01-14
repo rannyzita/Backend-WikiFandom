8a6188b5343f28f73feb1a07437c78fc3f148030
Kariny <karinysilva.estudo@gmail.com>
2025-01-13 22:34:49 -0300

    Implementação do GaleriaController com métodos para listar, criar e excluir imagens

---
+++ src/controllers/GaleriaController.js
@@ -0,0 +1,45 @@
+const GaleriaRepository = require('../models/GaleriaRepository.js');
+
+class GaleriaController{
+    static async getAllImages(req, res){
+        try{
+            const images = await GaleriaRepository.findAll();
+            res.json(images);
+        } catch(error){
+            return res.status(500).json({message: 'Erro ao verificar imagens', error});
+        }
+    }
+
+    static async createImage(req, res){
+        try{
+            const {titulo, url, categoria} = req.body;
+            if(!titulo || !url || !categoria){
+                return res.status(400).json({message: 'Erro ao criar imagem, insira o que é pedido nos campos corretamente.'});
+            }
+
+            const newImage = await GaleriaRepository.create({ titulo, url, categoria });
+
+            res.status(201).json({ message: 'Imagem criada.', imagem: newImage });
+        } catch (erro) {
+            return res.status(500).json({ message: 'Erro na criação da sua imagem', erro });
+        }
+    }
+
+    static async deleteImage(req, res){
+        try{
+            const imagemId = req.params.imagemId;
+            const image = await GaleriaRepository.findById(imagemId);
+
+            if(!image){
+                return res.status(404).json({ message: 'Imagem não encontrada'});
+            }
+
+            await GaleriaRepository.delete(imagemId);
+            res.status(201).json({message: 'Erro ao deletar imagem.'});
+        } catch(erro){
+            return res.status(500).json({ message: '', erro });
+        }
+    }
+}
+
+module.exports = GaleriaController;
