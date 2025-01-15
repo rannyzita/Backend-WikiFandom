a1b3b945ab68db8f16bd2440ed5f6a43b0fa00e3
Kariny <karinysilva.estudo@gmail.com>
2025-01-15 19:19:32 -0300

    Adicionando funcionalidades em Notícia

--- src/controllers/NoticiaController.js
+++ src/controllers/NoticiaController.js
@@ -1,27 +1,51 @@
-const NoticiaRepository = require('../models/NoticiaRepository.js');
+const NoticiaModel = require('../models/NoticiaRepository.js');
 
-class NoticiaRepository{
-    static async getAllNoticias(req, res){
-        try{
-
-        } catch(erro){
-
+class NoticiaRepository {
+    static async getAllNoticias(req, res) {
+        try {
+            const news = await NoticiaRepository.findAll();
+            res.json(news);
+        } catch (erro) {
+            console.error('Erro ao verificar notícias:', erro);
+            return res.status(500).json({ message: 'Erro ao verificar notícias.', erro });
         }
     }
 
-    static async createNoticia(req, res){
-        try{
+    static async createNoticia(req, res) {
+        try {
+            const {data} = req.body;
 
-        } catch(erro){
+            if (!data) {
+                return res.status(400).json({ message: 'Erro, data inválida.' });
+            }
 
+            const noticia = await NoticiaRepository.createNoticia({ data });
+            return res.status(201).json({ message: 'Notícia criada com sucesso.', data: noticia });
+        } catch (erro) {
+            console.error('Erro ao criar notícia:', erro);
+            return res.status(500).json({ message: 'Erro ao criar notícia.', erro });
         }
     }
 
-    static async deleteNoticia(req, res){
-        try{
+    static async deleteNoticia(req, res) {
+        try {
+            const { id } = req.params;
+            if (!id) {
+                return res.status(400).json({ message: 'Id da notícia não foi informado.' });
+            }
 
-        } catch(erro){
+            const deleta = await NoticiaModel.delete(id);
+            if (deleta) {
+                res.json({ message: 'Notícia deletada com sucesso.' });
+            } else {
+                res.status(404).json({ message: 'Notícia não encontrada.' });
+            }
             
+        } catch (erro) {
+            console.error('Erro ao deletar notícia:', erro);
+            return res.status(500).json({ message: 'Erro ao deletar notícia.', erro });
         }
     }
 }
+
+module.exports = NoticiaRepository;
