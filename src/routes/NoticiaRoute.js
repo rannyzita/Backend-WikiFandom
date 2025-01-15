cb3583fba174666b4c75f7d19158afaa1140d479
Kariny <karinysilva.estudo@gmail.com>
2025-01-15 19:20:51 -0300

    Adicionando rotas de notícia

--- src/routes/NoticiaRoute.js
+++ src/routes/NoticiaRoute.js
@@ -1,1 +1,9 @@
-// vou fazer ainda
+const express = require('express');
+const router = express.Router();
+const NoticiaController = require('../controllers/NoticiaController.js');
+
+router.get('/news', NoticiaController.getAllNoticias);
+router.post('/news', NoticiaController.createNoticia);
+router.delete('/news/:id', NoticiaController.deleteNoticia);
+
+module.exports = router;
