e339285f99c0ba45e7785fc40612dd9bfda0d60e
rannyzita <fs.vitoria.soares@gmail.com>
2025-01-16 10:39:52 -0300

    codigo do server

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ const cors = require("cors");
 const routeUsuario = require("./routes/UsuarioRoute.js");
 const app = express();
 
+// Middleware
 app.use(cors());
-
 app.use(express.json());
+
+// Rotas
 app.use("/api/usuarios", routeUsuario);
 
-app.use(routeUsuario);
-
-app.listen(3333);
+// Iniciar o servidor
+const PORT = 3333;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
