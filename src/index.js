b8dddf8d8f6cef3bbd04d9adfb0df79a2c650684
Kariny <karinysilva.estudo@gmail.com>
2025-01-16 10:41:24 -0300

    ...

--- src/index.js
+++ src/index.js
@@ -2,15 +2,17 @@ const express = require("express");
 const cors = require("cors");
 const routeUsuario = require("./routes/UsuarioRoute.js");
 const app = express();
-const authRoutes = require('./routes/authRoutes');
-app.use('/auth', authRoutes);
 
+// Middleware
 app.use(cors());
-
 app.use(express.json());
+
+// Rotas
 app.use("/api/usuarios", routeUsuario);
 
-app.use(routeUsuario);
-
-app.listen(3333);
+// Iniciar o servidor
+const PORT = 3333;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
 
