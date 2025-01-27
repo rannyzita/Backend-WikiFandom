d87b9c8f932461288690ad868fa61c75880afd3f
rannyzita <fs.vitoria.soares@gmail.com>
2025-01-27 16:23:27 -0300

    adicionando umas condições aqui

--- src/controllers/HistoricoPesquisaController.js
+++ src/controllers/HistoricoPesquisaController.js
@@ -14,12 +14,30 @@ class HistoricoPesquisaController{
     // Criar histórico
     static async createHistorico(req, res){
         try {
-            console.log('Received data:', req.body); 
             const createHistorico = await HistoricoPesquisaRepository.create(req.body);
             return res.status(201).json({ message: 'Historico realizado com sucesso', createHistorico });
         } catch (erro) {
-            console.error('Erro ao criar histórico', erro);
             return res.status(500).json({ message: 'Carregamento do histórico falhou.', erro: erro.message });
+        }
+    }
+
+    static async deleteHistorico(req, res){
+        try {
+            const {id} = req.body;
+
+            if(!id) {
+                return res.status(400).json({ message: 'ID do histórico é obrigatório.' });
+            }
+
+            const deleteHistorico = await HistoricoPesquisaRepository.delete(id);
+
+            if (deleteHistorico) {
+                res.json({ message: "historico apagado."});
+            } else {
+                res.status(404).json({ message: "Historico não encontrado." })
+            }
+        } catch (error) {
+            return res.status(404).json({ message: "Error: " + error.message });
         }
     }
 }
