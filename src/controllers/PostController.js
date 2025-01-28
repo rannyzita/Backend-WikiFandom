49c6e9fd786ee9adc0e96c76a99ec95f369eeb6b
rannyzita <fs.vitoria.soares@gmail.com>
2025-01-27 22:25:35 -0300

    adicionando mais status, e removendo umas funções desnecessarias do post controller

--- src/controllers/PostController.js
+++ src/controllers/PostController.js
@@ -49,51 +49,6 @@ class PostController {
             res.status(500).json({ error: err.message });
         }
     }
-
-    // Adicionando (Busca de posts por título ou conteúdo) - 22/01
-    static async buscarPost(req, res) {
-        // parâmetro
-        const {query} = req.query;
-        if (!query){
-            return res.status(400).json({message: "É necessário informar o termo de busca"});
-        }
-
-        try{
-            //parte do banco de dados
-            const post  = await knex('Post').where('titulo', 'like', `%${query}%`).orWhere('conteudo', 'like', `%${query}%`);
-            return res.json(post);
-        } catch(e){
-            return res.status(500).json({error: e.message})
-        }
-    }
-
-    // Adicionando Comentários em Post
-    static async addComentarioPost(req, res) {
-        // para eu fazer um comentário eu tenho que ter um id(cliente) e um conteúdo(mensagem)
-        const {id} = req.params; 
-        const {conteudo, IdUsuario} = req.body;
-
-        if(!conteudo || !IdUsuario){
-            return res.status(400).json({message: 'É preciso estar logado e ter conteúdo em sua mensagem para realizar um comentário'});
-        }
-
-        try{
-            const verificarPost = await knex('Post').where('id', id).first();
-            if(!verificarPost){
-                return res.status(400).json({message: 'Post não existente.'})
-            } 
-
-            await knex ('Comentario').insert({
-                conteudo,
-                id_usuario: IdUsuario,
-                id_post : id,
-                data_enviada:  knex.fn.now()
-            })
-            return res.status(201).json({message: 'Comentário adicionado.'})
-        } catch(e){
-            return res.status(500).json({error: e.message})
-        }
-    }
 }
 
 module.exports = PostController;
