67804bb8328c495f9c40743dda6660ec9a528f7e
rannyzita <fs.vitoria.soares@gmail.com>
2025-01-27 21:58:53 -0300

    adicionando comentario no usuarioModel

--- src/routes/ComentarioRoute.js
+++ src/routes/ComentarioRoute.js
@@ -3,9 +3,17 @@ const router = express.Router();
 const ComentarioController = require('../controllers/ComentarioController.js');
 
 // Usando os métodos do controller nas rotas
+
+// todos os comentarios existentes do site
 router.get('/comentarios', ComentarioController.getAllComentarios);
+
+// todos os comentarios do usuario em especifico
 router.get('/comentarios/:id', ComentarioController.getAllComentarioById);
+
+// criar um comentario no site
 router.post('/comentarios', ComentarioController.createComentario);
+
+// deletar um comentario
 router.delete('/comentarios/:id', ComentarioController.deleteComentario);
 
 module.exports = router;
