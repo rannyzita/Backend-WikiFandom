faea1e938fc699d6490ff52a04767cef078c41fa
rannyzita <fs.vitoria.soares@gmail.com>
2025-01-28 22:05:36 -0300

    mandando a logica de entrada

--- src/controllers/PostController.js
+++ src/controllers/PostController.js
@@ -31,6 +31,20 @@ class PostController {
     // Método para adicionar um novo post
     static async createPost(req, res) {
         try {
+            const usuario = await UsuarioRepository.findById(req.body.id_usuario);
+
+            if(!usuario) {
+                res.status(404).json({ message: "Usuário não encontrado." });
+                return;  // Evita que o restante da função seja executado se um erro ocorreu.
+            }
+
+            const imagem = await ImagemRepository.findById(req.body.id_imagem);
+
+            if (!imagem) {
+                res.status(404).json({ message: "Imagem não encontrada." });
+                return;  // Evita que o restante da função seja executado se um erro ocorreu.
+            }
+            
             const post = await PostRepository.create(req.body);
 
             if(!post) {
