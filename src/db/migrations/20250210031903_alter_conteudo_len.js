8eb44a8103681458a36dde4fd58067f1af486bb1
Kariny <karinysilva.estudo@gmail.com>
2025-02-10 00:36:18 -0300

    atualizando a main

---
+++ src/db/migrations/20250210031903_alter_conteudo_len.js
@@ -0,0 +1,18 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.table('Post', function(table) {
+        table.string('conteudo', 3100).alter(); 
+        table.string('titulo', 100).alter();// Altera o tamanho da coluna 'url_post'
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  
+};
