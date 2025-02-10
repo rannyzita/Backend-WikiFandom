bce32ece9768c5b8587812c4f7d05baff97efbaa
Kariny <karinysilva.estudo@gmail.com>
2025-02-10 00:57:56 -0300

    testando de novo

--- src/db/migrations/20250210031903_alter_conteudo_len.js
+++ src/db/migrations/20250210031903_alter_conteudo_len.js
@@ -2,6 +2,8 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
+
+// alterando tabela de post
 exports.up = function(knex) {
     return knex.schema.table('Post', function(table) {
         table.string('conteudo', 3100).alter(); 
