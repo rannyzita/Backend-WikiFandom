294efeb797378fd43a4e1683435019f4d23b3071
Kariny <karinysilva.estudo@gmail.com>
2025-02-10 10:00:31 -0300

    Terminando de adicionar os dados no banco

---
+++ src/db/migrations/20250210115044_alter_titulo_noticia.js
@@ -0,0 +1,17 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.table('Noticia', function(table) {
+        table.string('titulo', 200).alter(); 
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  
+};
